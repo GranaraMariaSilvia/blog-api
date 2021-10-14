0a11d185b941762070d5663a6377af2e10508d28
Maria Silvia Granara <granarasilvia10@gmail.com>
2021-10-14 17:18:53 -0300

    cambios

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+dotenv.config();
+
+app.use(express.json()); //para evitar el error en postman
+
+mongoose
+  .connect(process.env.MONGO_URL) //aqui se elimino el  usenewurlparcer, usecreateindex
+  .then(console.log("conectado a MongoDB"))
+  .catch((error) => console.log(error));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/servidor/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("el archivo se a subido");
+});
+
+app.use("/servidor/auth", authRoute);
+app.use("/servidor/users", userRoute);
+app.use("/servidor/posts", postRoute);
+app.use("/servidor/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend corriendo en el puerto 5000");
+});
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//Register
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+    const user = await newUser.save(); //guardo el nuevo usuario creado
+    res.status(200).json(user); //me muestra el usuario creado
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username }); //que busque un usuario con este nombre unico
+    !user && res.status(400).json("usuario inexistente"); //valido si existe este usuario
+
+    const validated = await bcrypt.compare(req.body.password, user.password); //aqui comparo las contraseñas
+    !validated && res.status(400).json("contraseña incorrecta"); // hago la validacion de la contraseña
+
+    const { password, ...others } = user._doc; //para que no muestre la contraseña
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
